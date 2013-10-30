20e801e948088a7ec4c073be93e07a74677f08d0
のぶ <nobuhiko.kimoto@gmail.com>
2013-10-30 19:25:28 +0900

    無駄なjsを削除

--- footer.php
+++ footer.php
@@ -50,7 +50,6 @@
         </ul>
     </div>
 </div>
-<?php endif; ?>
 <script type="text/javascript">
 (function() {
     var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
@@ -60,6 +59,7 @@
 </script>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 <script type="text/javascript" src="http://b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async"></script>
+<?php endif; ?>
 
 </body>
 </html>

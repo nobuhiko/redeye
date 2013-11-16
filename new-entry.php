d9640cff5185d78bb05458be29699eb26637eae2
のぶ <nobuhiko.kimoto@gmail.com>
2013-11-16 12:07:58 +0900

    新着記事のロジックを変更

--- new-entry.php
+++ new-entry.php
@@ -1,29 +1,22 @@
 <h4 class="menu_underh2">新着エントリー</h4>
 <div class="topnews">
-    <?php
-    $newslist = get_posts( array('posts_per_page' => 5 ));
-    foreach( $newslist as $post ) :
-        setup_postdata( $post );
-    ?>
+    <?php query_posts('showposts=5'); ?>
+    <?php while (have_posts()) : the_post(); ?>
     <dl>
         <dt>
-            <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
+        <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
             <?php if ( has_post_thumbnail() ): ?>
             <?php the_post_thumbnail('thumb100'); ?>
             <?php else: ?>
             <img src="<?php echo get_template_directory_uri(); ?>/images/no-img.png" alt="no image" title="no image" width="100" height="100">
             <?php endif; ?>
-            </a>
+        </a>
         </dt>
         <dd>
-            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-            <p><?php echo mb_substr(strip_tags(get_the_excerpt()),0,15).'...'; ?></p>
+        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+        <p><?php echo mb_substr(strip_tags(get_the_excerpt()),0,25).'...'; ?></p>
         </dd>
     </dl>
-
-    <?php
-    endforeach;
-    wp_reset_postdata();
-    ?>
+    <?php endwhile; wp_reset_postdata(); ?>
     <p class="motto"><a href="<?php echo home_url(); ?>/">→もっと見る</a></p>
 </div>

4fbc3315e07e8ca37e32a7b24526ac652d1eebed
のぶ <nobuhiko.kimoto@gmail.com>
2014-02-01 19:28:06 +0900

    構造化データを整理

--- functions.php
+++ functions.php
@@ -1,38 +1,38 @@
 <?php
 /* カテゴリータグを全部まとめて表示する */
 function get_the_taxonomies_st($post = 0, $args = array() ) {
-	$post = get_post( $post );
+    $post = get_post( $post );
 
-	$args = wp_parse_args( $args, array(
-		'template' => '%l',
-	) );
-	extract( $args, EXTR_SKIP );
+    $args = wp_parse_args( $args, array(
+        'template' => '%l',
+    ) );
+    extract( $args, EXTR_SKIP );
 
-	$taxonomies = array();
+    $taxonomies = array();
 
-	if ( !$post )
-		return $taxonomies;
+    if ( !$post )
+        return $taxonomies;
 
-	foreach ( get_object_taxonomies($post) as $taxonomy ) {
+    foreach ( get_object_taxonomies($post) as $taxonomy ) {
         $t = (array) get_taxonomy($taxonomy);
-		if ( empty($t['args']) )
-			$t['args'] = array();
-		if ( empty($t['template']) )
-			$t['template'] = $template;
+        if ( empty($t['args']) )
+            $t['args'] = array();
+        if ( empty($t['template']) )
+            $t['template'] = $template;
 
-		$terms = get_object_term_cache($post->ID, $taxonomy);
-		if ( false === $terms )
-			$terms = wp_get_object_terms($post->ID, $taxonomy, $t['args']);
+        $terms = get_object_term_cache($post->ID, $taxonomy);
+        if ( false === $terms )
+            $terms = wp_get_object_terms($post->ID, $taxonomy, $t['args']);
 
-		$links = array();
+        $links = array();
 
-		foreach ( $terms as $term )
-			$links[] = "<a href='" . esc_attr( get_term_link($term) ) . "'>$term->name</a>";
+        foreach ( $terms as $term )
+            $links[] = "<a href='" . esc_attr( get_term_link($term) ) . "'>$term->name</a>";
 
-		if ( $links )
-			$taxonomies[$taxonomy] = implode(', ', $links);
-	}
-	return $taxonomies;
+        if ( $links )
+            $taxonomies[$taxonomy] = implode(', ', $links);
+    }
+    return $taxonomies;
 }
 
 function is_mobile () {
@@ -60,8 +60,8 @@ function register_user_script() {
     if (!is_admin()) {
         $script_directory = get_template_directory_uri();
         wp_deregister_script('jquery');
-	    wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"), false, '1.8.0');
-    	wp_enqueue_script('jquery');
+        wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"), false, '1.8.0');
+        wp_enqueue_script('jquery');
 
         if (!is_mobile()) {
             wp_enqueue_script(
@@ -93,9 +93,9 @@ register_nav_menus(array('navbar' => 'ナビゲーションバー'));
 
 //カスタムヘッダー
 $args = array(
-	'width'         => 950,
-	'height'        => 150,
-	'default-image' => '',
+    'width'         => 950,
+    'height'        => 150,
+    'default-image' => '',
 );
 add_theme_support( 'custom-header', $args );
 
@@ -106,8 +106,8 @@ add_theme_support('automatic-feed-links');
 add_theme_support('editor-style');
 add_editor_style('editor-style.css');
 function custom_editor_settings( $initArray ){
-	$initArray['body_class'] = 'editor-area';
-	return $initArray;
+    $initArray['body_class'] = 'editor-area';
+    return $initArray;
 }
 add_filter( 'tiny_mce_before_init', 'custom_editor_settings' );
 
@@ -126,51 +126,51 @@ add_theme_support( 'custom-background' );
 //ページャー機能
 function pagination($pages = '', $range = 4)
 {
-     $showitems = ($range * 2)+1;
+    $showitems = ($range * 2)+1;
 
-     global $paged;
-     if(empty($paged)) $paged = 1;
+    global $paged;
+    if(empty($paged)) $paged = 1;
 
-     if($pages == '')
-     {
-         global $wp_query;
-         $pages = $wp_query->max_num_pages;
-         if(!$pages)
-         {
-             $pages = 1;
-         }
-     }
+    if($pages == '')
+    {
+        global $wp_query;
+        $pages = $wp_query->max_num_pages;
+        if(!$pages)
+        {
+            $pages = 1;
+        }
+    }
 
-     if(1 != $pages)
-     {
-         echo "<div class=\"pagination\"><span>Page ".$paged." of ".$pages."</span>";
-         if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<a href='".get_pagenum_link
+    if(1 != $pages)
+    {
+        echo "<div class=\"pagination\"><span>Page ".$paged." of ".$pages."</span>";
+        if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<a href='".get_pagenum_link
 
-(1)."'>&laquo; First</a>";
-         if($paged > 1 && $showitems < $pages) echo "<a href='".get_pagenum_link($paged - 1)."'>&lsaquo;
+            (1)."'>&laquo; First</a>";
+        if($paged > 1 && $showitems < $pages) echo "<a href='".get_pagenum_link($paged - 1)."'>&lsaquo;
 
-Previous</a>";
+        Previous</a>";
 
-         for ($i=1; $i <= $pages; $i++)
-         {
-             if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems
+        for ($i=1; $i <= $pages; $i++)
+        {
+            if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems
 
-))
-             {
-                 echo ($paged == $i)? "<span class=\"current\">".$i."</span>":"<a href='".get_pagenum_link
+            ))
+            {
+                echo ($paged == $i)? "<span class=\"current\">".$i."</span>":"<a href='".get_pagenum_link
 
-($i)."' class=\"inactive\">".$i."</a>";
-             }
-         }
+                    ($i)."' class=\"inactive\">".$i."</a>";
+            }
+        }
 
-         if ($paged < $pages && $showitems < $pages) echo "<a href=\"".get_pagenum_link($paged +
+        if ($paged < $pages && $showitems < $pages) echo "<a href=\"".get_pagenum_link($paged +
 
-1)."\">Next &rsaquo;</a>";
-         if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a
+            1)."\">Next &rsaquo;</a>";
+        if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a
 
-href='".get_pagenum_link($pages)."'>Last &raquo;</a>";
-         echo "</div>\n";
-     }
+            href='".get_pagenum_link($pages)."'>Last &raquo;</a>";
+        echo "</div>\n";
+    }
 }
 
 //ヘッダーを綺麗に
@@ -203,38 +203,38 @@ add_action( 'pre_ping', 'red_no_self_ping' );
 
 //ウイジェット追加
 if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar(4) )
+    register_sidebars(1,
+        array(
+            'name'=>'サイドバー1',
+            'before_widget' => '<ul><li>',
+            'after_widget' => '</li></ul>',
+            'before_title' => '<h4 class="menu_underh2">',
+            'after_title' => '</h4>',
+        ));
 register_sidebars(1,
     array(
-    'name'=>'サイドバー1',
-    'before_widget' => '<ul><li>',
-    'after_widget' => '</li></ul>',
-    'before_title' => '<h4 class="menu_underh2">',
-    'after_title' => '</h4>',
+        'name'=>'スクロール広告用',
+        'before_widget' => '<ul><li>',
+        'after_widget' => '</li></ul>',
+        'before_title' => '<h4 class="menu_underh2" style="text-align:left;">',
+        'after_title' => '</h4>',
     ));
 register_sidebars(1,
     array(
-    'name'=>'スクロール広告用',
-    'before_widget' => '<ul><li>',
-    'after_widget' => '</li></ul>',
-    'before_title' => '<h4 class="menu_underh2" style="text-align:left;">',
-    'after_title' => '</h4>',
-    ));
-register_sidebars(1,
-    array(
-    'name'=>'Googleアドセンス用',
-    'before_widget' => '',
-    'after_widget' => '',
-    'before_title' => '<h4 style="display:none">',
-    'after_title' => '</h4>',
+        'name'=>'Googleアドセンス用',
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title' => '<h4 style="display:none">',
+        'after_title' => '</h4>',
     ));
 
 register_sidebars(1,
     array(
-    'name'=>'Googleアドセンスのスマホ用width300',
-    'before_widget' => '',
-    'after_widget' => '',
-    'before_title' => '<h4 style="display:none">',
-    'after_title' => '</h4>',
+        'name'=>'Googleアドセンスのスマホ用width300',
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title' => '<h4 style="display:none">',
+        'after_title' => '</h4>',
     ));
 
 if ( ! isset( $content_width ) ) $content_width = 546;
@@ -274,10 +274,10 @@ function get_current_category()
 }
 
 function get_top_category( $cat_id, $ancestors = array() ) {
-	$cat = get_category( $cat_id );
-	$ancestors = $cat;
-	if ( $cat->parent != 0 ) {
-		$ancestors = get_top_category( $cat->parent, $ancestors );
-	}
-	return $ancestors;
+    $cat = get_category( $cat_id );
+    $ancestors = $cat;
+    if ( $cat->parent != 0 ) {
+        $ancestors = get_top_category( $cat->parent, $ancestors );
+    }
+    return $ancestors;
 }
--- single.php
+++ single.php
@@ -1,101 +1,99 @@
 <?php get_header(); ?>
 <div class="kuzu">
-<?php /*--- パンくず --- */?>
-<?php if ( function_exists( 'bread_crumb' ) ) { bread_crumb(); } ?>
+    <?php /*--- パンくず --- */?>
+    <?php if ( function_exists( 'bread_crumb' ) ) { bread_crumb(); } ?>
 </div><!--/kuzu-->
 <div id="post-<?php the_ID(); ?>" <?php post_class('post'); ?>>
-<!--ループ開始-->
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<div class="kizi">
+    <!--ループ開始-->
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <div class="kizi" itemscope itemtype="http://schema.org/Article">
+        <h1 class="entry-title" itemprop="name"><a href="<?php the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></h1>
+        <div class="blog_info contentsbox">
+            <p>
+            &nbsp;公開日：<time class="published" itemprop="datePublished" content="<?php the_time('c');?>"><?php the_time('Y年n月j日 H:i') ;?></time>
+            </p>
+        </div>
+        <div itemprop="articleBody" class="entry-content">
+            <?php the_content(); ?>
+        </div>
+    </div>
+    <div style="padding:20px 0px;">
+        <?php get_template_part('ad');?>
+    </div>
 
-<h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <div id="snsbox03">
+        <div class="sns03">
+            <ul class="snsb clearfix">
+                <li><a href="https://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="" data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>">Tweet</a></li>
+                <li><div class="fb-like" data-href="<?php the_permalink();?>" data-width="70" data-height="62" data-colorscheme="light" data-layout="box_count" data-action="like" data-show-faces="false" data-send="false"></div></li>
+                <li></script><g:plusone size="tall" href="<?php the_permalink(); ?>"></g:plusone></li>
+                <li><a href="http://b.hatena.ne.jp/entry/<?php the_permalink(); ?>" class="hatena-bookmark-button" data-hatena-bookmark-title="<?php the_title(); ?>｜<?php bloginfo('name'); ?>" data-hatena-bookmark-layout="vertical" title="このエントリーをはてなブックマークに追加"><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a></li>
+                <li>
+                <a href='http://cloud.feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Fnob-log.info%2Ffeed%2F'  target='blank'><img id='feedlyFollow' src='http://s3.feedly.com/img/follows/feedly-follow-rectangle-volume-big_2x.png' alt='follow us in feedly' width='131' height='56'></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="blog_info contentsbox">
+        <p>
+        <span class="vcard author">
+            <span class="fn"><?php the_author_posts_link(); ?></span>
+        </span>
+        最終更新：<time class="updated" itemprop="dateModified" content="<?php the_modified_date('c'); ?>"><?php the_modified_date(); ?>&nbsp;<?php the_modified_date('G:i'); ?></time>
+        <span itemprop="keywords"><?php echo implode(' ', array_values(get_the_taxonomies_st())); ?></span>
+        </p>
+    </div>
+    <div style="padding:20px 0px;">
+        <?php get_template_part('ad');?>
+    </div>
+    <?php endwhile; else: ?>
+    <p>記事がありません</p>
+    <?php endif; ?>
+    <!--ループ終了-->
+    <div class="kizi02">
 
-<div class="blogbox">
-<p>
-<span class="kdate">投稿日：<time class="entry-date" datetime="<?php the_time('c') ;?>"><?php the_date(); ?> <?php the_time();?></time></span>
-</p>
-</div>
-
-<div>
-<?php the_content(); ?>
-</div>
-
-<?php /*wp_link_pages();*/ ?>
-</div>
-<div style="padding:20px 0px;">
-<?php get_template_part('ad');?>
-</div>
-
-<div id="snsbox03">
-<div class="sns03">
-<ul class="snsb clearfix">
-<li><a href="https://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="" data-url="<?php the_permalink(); ?>" data-text="<?php the_title(); ?>">Tweet</a></li>
-<li><div class="fb-like" data-href="<?php the_permalink();?>" data-width="70" data-height="62" data-colorscheme="light" data-layout="box_count" data-action="like" data-show-faces="false" data-send="false"></div></li>
-<li></script><g:plusone size="tall" href="<?php the_permalink(); ?>"></g:plusone></li>
-<li><a href="http://b.hatena.ne.jp/entry/<?php the_permalink(); ?>" class="hatena-bookmark-button" data-hatena-bookmark-title="<?php the_title(); ?>｜<?php bloginfo('name'); ?>" data-hatena-bookmark-layout="vertical" title="このエントリーをはてなブックマークに追加"><img src="http://b.st-hatena.com/images/entry-button/button-only.gif" alt="このエントリーをはてなブックマークに追加" width="20" height="20" style="border: none;" /></a></li>
-<li>
-<a href='http://cloud.feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Fnob-log.info%2Ffeed%2F'  target='blank'><img id='feedlyFollow' src='http://s3.feedly.com/img/follows/feedly-follow-rectangle-volume-big_2x.png' alt='follow us in feedly' width='131' height='56'></a>
-</li>
-</ul>
-</div>
-</div>
-<div class="blog_info contentsbox">
-<p>
-<span itemprop="keywords"><?php echo implode(' ', array_values(get_the_taxonomies_st())); ?></span>
-</p>
-</div>
-<div style="padding:20px 0px;">
-<?php get_template_part('ad');?>
-</div>
-<?php endwhile; else: ?>
-<p>記事がありません</p>
-<?php endif; ?>
-<!--ループ終了-->
-<div class="kizi02">
-
-<!--関連記事-->
+        <!--関連記事-->
 <?php if (function_exists('st_get_related_posts')) :
 $related_posts = st_get_related_posts('number=5&title=&include_page=false&format=array');
 if ( is_array($related_posts) ) :
 ?>
-<h4 class="kanren">関連記事</h4>
-<div class="sumbox02">
-<div class="topnews">
-    <?php foreach ( $related_posts as $related ) : ?>
-    <dl>
-        <dt>
-        <a href="<?= get_permalink($related->ID); ?>" title="<?= $related->post_title; ?>">
-            <?php if (has_post_thumbnail($related->ID)): ?>
-                <?= get_the_post_thumbnail($related->ID, 'thumb110'); ?>
-            <?php else: ?>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/no-img.png" alt="no image" title="no image" width="110" height="110">
-            <?php endif; ?>
-            </a>
-        </dt>
-        <dd>
-            <a href="<?= get_permalink($related->ID); ?>"><?= $related->post_title; ?></a>
-            <p class="basui">
-            <?php echo mb_substr(strip_tags($related->post_content),0,35).'...'; ?></p>
-            <p class="motto"><a href="<?= get_permalink($related->ID); ?>">記事を読む</a></p>
-        </dd>
-    </dl>
-    <?php endforeach; ?>
-</div>
-</div>
-<!--/関連記事-->
-<?php endif; ?>
-<?php endif; ?>
+        <h4 class="kanren">関連記事</h4>
+        <div class="sumbox02">
+            <div class="topnews">
+                <?php foreach ( $related_posts as $related ) : ?>
+                <dl>
+                    <dt>
+                    <a href="<?= get_permalink($related->ID); ?>" title="<?= $related->post_title; ?>">
+                        <?php if (has_post_thumbnail($related->ID)): ?>
+                        <?= get_the_post_thumbnail($related->ID, 'thumb110'); ?>
+                        <?php else: ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/no-img.png" alt="no image" title="no image" width="110" height="110">
+                        <?php endif; ?>
+                    </a>
+                    </dt>
+                    <dd>
+                    <a href="<?= get_permalink($related->ID); ?>"><?= $related->post_title; ?></a>
+                    <p class="basui">
+                    <?php echo mb_substr(strip_tags($related->post_content),0,35).'...'; ?></p>
+                    <p class="motto"><a href="<?= get_permalink($related->ID); ?>">記事を読む</a></p>
+                    </dd>
+                </dl>
+                <?php endforeach; ?>
+            </div>
+        </div>
+        <!--/関連記事-->
+        <?php endif; ?>
+        <?php endif; ?>
 
-</div><!--/kizi-->
-<?php comments_template(); ?>
-<!--ページナビ-->
-<div class="p-navi">
-<p>
-PREV : <?php previous_post_link_plus( array('in_same_cat' => true, 'format' => '%link') );?><br/>
-NEXT : <?php next_post_link_plus( array('in_same_cat' => true, 'format' => '%link') );?></p>
-</div>
-<?php if (!is_mobile ()) : ?>
-<!-- X:S ZenBackWidget --><div id="zenback-widget-loader"></div><script type="text/javascript">!function(d,i){if(!d.getElementById(i)){var r=Math.ceil((new Date()*1)*Math.random());var j=d.createElement("script");j.id=i;j.async=true;j.src="//w.zenback.jp/v1/?base_uri=http%3A//nob-log.info&nsid=111935142657809798%3A%3A111935150442471907&rand="+r;d.body.appendChild(j);}}(document,"zenback-widget-js");</script><!-- X:E ZenBackWidget -->
-<?php endif; ?>
+    </div><!--/kizi-->
+    <?php comments_template(); ?>
+    <!--ページナビ-->
+    <div class="p-navi">
+        <?php previous_post_link_plus( array('in_same_cat' => true, 'format' => '<p>PREV : %link</p>') );?>
+        <?php next_post_link_plus( array('in_same_cat' => true, 'format' => '<p>NEXT : %link</p>') );?>
+    </div>
+    <?php if (!is_mobile ()) : ?>
+    <!-- X:S ZenBackWidget --><div id="zenback-widget-loader"></div><script type="text/javascript">!function(d,i){if(!d.getElementById(i)){var r=Math.ceil((new Date()*1)*Math.random());var j=d.createElement("script");j.id=i;j.async=true;j.src="//w.zenback.jp/v1/?base_uri=http%3A//nob-log.info&nsid=111935142657809798%3A%3A111935150442471907&rand="+r;d.body.appendChild(j);}}(document,"zenback-widget-js");</script><!-- X:E ZenBackWidget -->
+    <?php endif; ?>
 </div><!-- END div.post -->
 <?php get_footer(); ?>
